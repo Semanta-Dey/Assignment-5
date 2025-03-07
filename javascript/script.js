f4dd6ce949bb13c7f4fe9da6ad9c18b0ccf62ee4
Semanta-Dey <semantadey2003@gmail.com>
2025-03-07 23:47:27 +0600

    non-functional work done

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,10 @@
 function updateDate() {
-    const now = new Date();
+    const present = new Date();
 
-    const optionsDay = { weekday: "short" }; // Example: "Wed"
-    const optionsDate = { month: "short", day: "2-digit", year: "numeric" }; // Example: "Jul 28 2025"
-
-    document.getElementById("day").innerText = now.toLocaleDateString("en-US", optionsDay) + ",";
-    document.getElementById("full-date").innerText = now.toLocaleDateString("en-US", optionsDate);
+    const optionsDay = { weekday: "short" }; 
+    const optionsDate = { month: "short", day: "2-digit", year: "numeric" }; 
+    document.getElementById("day").innerText = present.toLocaleDateString("en-US", optionsDay) + ",";
+    document.getElementById("full-date").innerText = present.toLocaleDateString("en-US", optionsDate);
 }
 
 document.addEventListener("DOMContentLoaded", updateDate);
