918c2276cae5a6fdf82ff2cdc91f83992f344afd
Semanta-Dey <semantadey2003@gmail.com>
2025-03-10 00:15:44 +0600

    all done

--- javascript/script.js
+++ javascript/script.js
@@ -44,38 +44,29 @@ document.querySelectorAll(".groupA").forEach(function(button) {
 });
 
 
-// document.addEventListener("DOMContentLoaded", function() {
-//     // the message box empty at the start
-//     document.getElementById("message").innerHTML = "";
-// });
+//By clicking clear history all the message are disappear
 
-// document.querySelectorAll(".groupA").forEach(function(button) {
-//     button.addEventListener("click", function(){
-//         const remainTask = parseInt(document.getElementById("task-number").innerText);
-//         if (remainTask > 0) {
-//             // alert("Board updated Successfully");
-//             const remainingTask = Math.max(0, remainTask - 1);
-//             const completedTask = parseInt(document.getElementById("task-completed-num").innerText) + 1;
-//             document.getElementById("task-number").innerText = String(remainingTask).padStart(2, '0');
-//             document.getElementById("task-completed-num").innerText = completedTask;
-
-//             // Disable the button when it is clicked
-//             this.disabled = true;
-//             let taskName = this.closest(".h-52").querySelector(".taskName").innerText;
-//             let currentTime = new Date().toLocaleTimeString();
-//             let messageBox = document.getElementById("message");
-//             let newMessage = document.createElement("p");
-//             newMessage.classList.add("mb-3", "p-2", "rounded-md", "bg-[rgb(220,236,255)]");
-//             newMessage.innerText = `You have completed the task ${taskName} at ${currentTime}`;
-//             messageBox.appendChild(newMessage);
-
-//             // Check if all tasks are completed
-//             if (remainingTask === 0) {
-//                 alert("Congrats! All tasks have been completed.");
-//             }
-//         }
-//     });
-// });
+document.getElementById("clear-history").addEventListener("click",function(){
+    document.getElementById("message").innerHTML="";
+});
 
 
+//Color Theme
+
+document.getElementById("color-picker").addEventListener("click",function(){
+    const random= Math.floor(Math.random()*5);
+    switch(random){
+        case 0:document.body.style.backgroundColor ="rgb(245, 216, 180)";
+        break;
+        case 1:document.body.style.backgroundColor = "rgb(211, 233, 139)";
+        break;
+        case 2:document.body.style.backgroundColor = "rgb(166, 255, 255)";
+        break;
+        case 3:document.body.style.backgroundColor = "rgb(122, 204, 232)";
+        break;
+        case 4:document.body.style.backgroundColor = "rgb(243, 169, 169)";
+        break;
+        default:document.body.style.backgroundColor = "rgb(220,236,255)";
+    }
+});
 
